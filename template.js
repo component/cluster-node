abb10541f26414ee53db4ff3a3a042d79448569b
TJ Holowaychuk <tj@vision-media.ca>
2013-05-01 20:05:43 -0700

    hide load avg for now

--- template.js
+++ template.js
@@ -1,1 +1,1 @@
-module.exports = '<div class="node">\n  <h2 data-text="name"></h2>\n\n  <div class="stats">\n    <span class="uptime" data-text="uptime"></span>\n    <span class="loadavg" data-text="loadavg"></span>\n  </div>\n\n\n  <ul class="processes">\n  </ul>\n</div>\n';
+module.exports = '<div class="node">\n  <h2 data-text="name"></h2>\n\n  <div class="stats">\n    <!-- <span class="uptime" data-text="uptime"></span> -->\n    <span class="loadavg" data-text="loadavg"></span>\n  </div>\n\n\n  <ul class="processes">\n  </ul>\n</div>\n';
